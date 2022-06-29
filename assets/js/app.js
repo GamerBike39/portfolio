398a776d6fd89e8133fad5e9d22887e078b4ad02
GamerBike39 <champidub@gmail.com>
2022-06-29 13:12:12 +0200

    changement d'idée pour l'animation

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+let projetOne = document.querySelector(".containerProject > h3:nth-child(1)");
+let projetTwo = document.querySelector(".containerProject > h3:nth-child(2)");
+let projetThree = document.querySelector(".containerProject > h3:nth-child(3)");
+
+projetOne.addEventListener("mouseenter", function () {
+  projetTwo.classList.add("opac");
+  projetThree.classList.add("opac");
+});
+
+projetOne.addEventListener("mouseleave", function () {
+  projetTwo.classList.remove("opac");
+  projetThree.classList.remove("opac");
+});
+
+projetTwo.addEventListener("mouseenter", function () {
+  projetOne.classList.add("opac");
+  projetThree.classList.add("opac");
+});
+projetTwo.addEventListener("mouseleave", function () {
+  projetOne.classList.remove("opac");
+  projetThree.classList.remove("opac");
+});
+
+projetThree.addEventListener("mouseenter", function () {
+  projetOne.classList.add("opac");
+  projetTwo.classList.add("opac");
+});
+projetThree.addEventListener("mouseleave", function () {
+  projetOne.classList.remove("opac");
+  projetTwo.classList.remove("opac");
+});
