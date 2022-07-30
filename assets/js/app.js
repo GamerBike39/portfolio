8b004a7a5ddc26bf5b6ead523d9fcdb66049af87
GamerBike39 <champidub@gmail.com>
2022-07-31 00:10:37 +0200

    refactorisation

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,9 @@ const projetThree = document.querySelector(".containerProject > h3:nth-child(3)"
 const projetFour = document.querySelector(".containerProject > h3:nth-child(4)");
 // ****modale****
 const btnPop = document.querySelector(".btn-modalPop");
-const btnAllo = document.querySelector(".btn-modalAllo");
-const btnCrea = document.querySelector(".btn-modalCrea");
-const btnBoots = document.querySelector(".btn-modalBoots");
-const modalPop = document.querySelector(".modalPop");
-const modalAllo = document.querySelector(".modalAllo");
-const modalCrea = document.querySelector(".modalCrea");
 const modalBoots = document.querySelector(".modalBoots");
-// const allModal = document.querySelectorAll(".modal");
 const modal = document.querySelector(".modal");
 const closeModalePop = document.querySelector(".closePop");
-const closeModaleAllo = document.querySelector(".closeAllo");
-const closeModaleCrea = document.querySelector(".closeCrea");
-const closeModaleBoots = document.querySelector(".closeBoots");
 const containerProject = document.querySelector(".containerProject");
 const dekstopNavModale = document.querySelector("header");
 // modal à remplir avec json
@@ -27,19 +17,18 @@ const modalGit = document.querySelector(".modalGit")
 const modalLink = document.querySelector(".modalLink");
 const modalBtn = document.querySelectorAll(".containerProject > h3");
 // ********* mouse enter and leave events ***********
-const animModalBtn = () => {
-  modalBtn.forEach((item) => {
-    item.addEventListener("mouseenter", () => {
-      for (let i = 0; i < modalBtn.length; i++) {
-        item.classList.add("opac");
-      }
-    });
-    item.addEventListener("mouseleave", () => {
-      item.classList.remove("opac");
-    });
-  });
-}
-animModalBtn();
+
+// const animModalBtn = () => {
+//   modalBtn.forEach((item) => {
+//     item.addEventListener("mouseover", () => {
+//       item.classList.add("opac");
+//     })
+//     item.addEventListener("mouseleave", () => {
+//       item.classList.remove("opac");
+//     })
+//   })
+// }
+// animModalBtn();
 
 
 
@@ -85,16 +74,19 @@ animModalBtn();
 // });
 
 
-closeModalePop.addEventListener("click", function () {
-  modalPop.classList.toggle("flex");
-  modalPop.classList.toggle("displayNone");
-  containerProject.classList.remove("scaleTranslate");
-  dekstopNavModale.classList.remove("dekstopNavModale");
-  dekstopNavModale.classList.remove("scaleTranslate");
-})
+const closeModale = function () {
+  closeModalePop.addEventListener("click", function () {
+    modal.classList.toggle("flex");
+    modal.classList.toggle("displayNone");
+    containerProject.classList.remove("scaleTranslate");
+    dekstopNavModale.classList.remove("dekstopNavModale");
+    dekstopNavModale.classList.remove("scaleTranslate");
+  })
+};
+closeModale();
 
 //**** */ effets 3d ****
-var ztxt = new Ztextify(".name", {
+const ztxt = new Ztextify(".name", {
   depth: "3px",
   layers: 8,
   event: "pointer",
@@ -238,13 +230,13 @@ new ResizeObserver((entries) => {
 // curseur custom
 
 const cursorTuning = () => {
-  var cursor = document.querySelector(".cursor");
-  var cursorinner = document.querySelector(".cursor2");
-  var a = document.querySelectorAll("a");
+  const cursor = document.querySelector(".cursor");
+  const cursorinner = document.querySelector(".cursor2");
+  const a = document.querySelectorAll("a");
 
   document.addEventListener("mousemove", function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
+    const x = e.clientX;
+    const y = e.clientY;
     cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${
     e.clientY
   }px - 50%), 0) rotate(${x + y}deg)`;
@@ -252,8 +244,8 @@ const cursorTuning = () => {
   });
 
   document.addEventListener("mousemove", function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
+    const x = e.clientX;
+    const y = e.clientY;
     cursorinner.style.left = x + "px";
     cursorinner.style.top = y + "px";
   });
@@ -277,7 +269,6 @@ const cursorTuning = () => {
     });
   });
 }
-
 cursorTuning();
 
 // rubrique contact form
@@ -298,6 +289,22 @@ function closeModalOpen() {
 closeModalOpen();
 
 
+// *****caroussele******
+const caroussele = () => {
+  let slider = tns({
+    container: ".my-slider",
+    items: 1,
+    gutter: 5,
+    mouseDrag: true,
+    swipeAngle: false,
+    speed: 400,
+    controlsText: ["<", ">"],
+    controlsPosition: "bottom",
+    loop: true,
+  });
+}
+
+
 
 // ******* construction des modales projets depuis JSON **************
 async function getProject() {
@@ -306,8 +313,8 @@ async function getProject() {
   for (let i = 0; i < data.length; i++) {
     // modalBtn[i].innerHTML = data[i].title;
     modalBtn[i].addEventListener("click", function () {
-      home.classList.add("scaleTranslate");
-      dekstopNavModale.classList.add("dekstopNavModale");
+      containerProject.classList.add("scaleTranslate");
+      dekstopNavModale.classList.add("dekstopNavModale", "scaleTranslate");
       if (data[i].title === modalBtn[i].textContent) {
         modal.classList.add("flex");
         modal.classList.remove("displayNone");
@@ -322,18 +329,7 @@ async function getProject() {
         <path
             d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z' /></a>`
         modalLink.href = data[i].link;
-
-        let slider = tns({
-          container: ".my-slider",
-          items: 1,
-          gutter: 5,
-          mouseDrag: true,
-          swipeAngle: false,
-          speed: 400,
-          controlsText: ["<", ">"],
-          controlsPosition: "bottom",
-          loop: true,
-        });
+        caroussele();
         return;
       }
     });
