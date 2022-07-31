53df3a93b4917c73d7d03f2a3794aef9f4f52343
GamerBike39 <champidub@gmail.com>
2022-07-31 15:44:58 +0200

    modalBtn active Ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -70,6 +70,9 @@ const closeModale = function () {
     modal.classList.toggle("flex");
     modal.classList.toggle("displayNone");
     containerProject.classList.remove("scaleTranslate");
+    modalBtn.forEach((item) => {
+      item.classList.remove("opac", "active");
+    })
     dekstopNavModale.classList.remove("dekstopNavModale");
     dekstopNavModale.classList.remove("scaleTranslate");
   })
@@ -248,7 +251,22 @@ const caroussele = () => {
 }
 
 // ***** anim btn modal ****
+const modalBtnActive = () => {
 
+  for (let i = 0; i < modalBtn.length; i++) {
+    if (containerProject.classList.contains("scaleTranslate") === false) {
+      modalBtn[i].classList.remove("opac");
+    }
+    modalBtn[i].addEventListener("click", function () {
+      for (let j = 0; j < modalBtn.length; j++) {
+        modalBtn[j].classList.add("opac");
+      }
+      modalBtn[i].classList.remove("opac");
+    });
+  }
+}
+
+modalBtnActive();
 // ******* construction des modales projets depuis JSON **************
 async function getProject() {
   const response = await fetch("assets/json/project.json");
@@ -262,14 +280,7 @@ async function getProject() {
       });
       containerProject.classList.add("scaleTranslate");
       dekstopNavModale.classList.add("dekstopNavModale", "scaleTranslate");
-
-      modalBtn[i].ariaChecked = "false";
-      modalBtn[i].classList.add("opac");
       if (data[i].title === modalBtn[i].textContent) {
-        modalBtn[i].ariaChecked = "true";
-        modalBtn[i].classList.remove("opac")
-        console.log(modalBtn[i]);
-        // modalBtn[i].classList.remove("opac");
         modal.classList.add("flex");
         modal.classList.remove("displayNone");
         modalTitle.innerHTML = data[i].title;
@@ -281,16 +292,8 @@ async function getProject() {
             d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z' /></a>`
         modalLink.href = data[i].link;
         caroussele();
+        modalBtnActive();
       }
-
-      if (data[i].title === modalBtn[i].textContent && modal.classList.contains("flex")) {
-        modalBtn[i].ariaChecked = "true";
-        modalBtn[i].classList.remove("opac");
-      } else {
-        modalBtn[i].ariaChecked = "false";
-        modalBtn[i].classList.add("opac");
-      }
-
     });
   }
 }
